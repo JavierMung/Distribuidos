de94c520aee5f4bcab13466c0809f1250b0a64f3
JavierMung <javiermunguia35@gmail.com>
2023-03-26 21:47:58 -0600

    Subo el programa

--- ServidorA.java
+++ ServidorA.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.DatagramPacket;
